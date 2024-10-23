7eb3eff7c0c2eeec1fef7f7cfad81fcf0a722a12
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-10-23 07:07:12 +0300

    refactor: refactored tests

--- src/cpu/tests.rs
+++ src/cpu/tests.rs
@@ -111,243 +111,223 @@ mod ldx {
     }
 }
 
-// #[test]
-// fn lda_test() {
-//     test_opcode(OpCode::LdxImmediate, &[0x1], 2, &[], |cpu, _memory| {
-//         assert_eq!(cpu.x_index, 1)
-//     });
+mod lda {
+    use super::*;
 
-//     let mut ram = Ram::new();
-//     let mut cpu = Cpu::new();
-//     let mut memory = MemoryMapping { ram: &mut ram };
-//     cpu.program_counter = 0;
+    #[test]
+    fn immediate() {
+        const TARGET: u8 = 0x42;
 
-//     #[rustfmt::skip]
-//     let mem_state = [
-//         // LDA #1
-//         0xA9, 0x1,
-//         // LDA $89
-//         0xA5, 0x89,
-//         // LDA $89, X       (X = 0x01)
-//         0xB5, 0x89,
-//         // LDA $89, X       (X = 0xB5) (test page overflow behavior)
-//         0xB5, 0x89,
-//         // LDA 0x0489
-//         0xAD, 0x89, 0x04,
-//         // LDA 0x0489, X    (X = 0x01)
-//         0xBD, 0x89, 0x04,
-//         // LDA 0x0489, X    (X = 0xB5) (test page overflow behavior)
-//         0xBD, 0x89, 0x04,
-//         // LDA 0x0489, Y    (Y = 0x02)
-//         0xB9, 0x89, 0x04,
-//         // LDA 0x0489, Y    (Y = 0xB6) (test page overflow behavior)
-//         0xB9, 0x89, 0x04,
-//         // LDA ($0x78, X)
-//         0xA1, 0x78,
-//         // LDA($0x77, X)
-//         0xA1, 0x77,
-//         // LDA($0x70),Y
-//         0xB1, 0x70,
-//         0xB1, 0x70,
-//     ];
-//     for (i, byte) in mem_state.into_iter().enumerate() {
-//         memory.store(i as u16, byte);
-//     }
-//     // zero page 0x89
-//     memory.store(0x0089, 0x02);
-//     // zero page 0x89 + 1
-//     memory.store(0x008A, 0x03);
-//     // zero page + 0xB5 (0x003E), page is wrapped around
-//     memory.store(0x003E, 0x04);
-//     // absolute 0x0489
-//     memory.store(0x0489, 0x05);
-//     // absolute 0x0489 + 1
-//     memory.store(0x048A, 0x06);
-//     // absolute 0x0489 + 0xB5
-//     memory.store(0x053E, 0x07);
-//     // absolute 0x0489 + 2
-//     memory.store(0x048B, 0x08);
-//     // absolute 0x0489 + 0xB6
-//     memory.store(0x053F, 0x09);
-//     // indirect 0x78 + 1 -> 0x0234,
-//     memory.store(0x0079, 0x34);
-//     memory.store(0x007A, 0x02);
-//     memory.store(0x0234, 0x0A);
-//     // indirect 0x77 + 255 -> 0x0235,
-//     memory.store(0x0076, 0x35);
-//     memory.store(0x0077, 0x02);
-//     memory.store(0x0235, 0x0B);
+        TestOpcodeOptions::new(OpCode::LdaImmediate, 2, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[TARGET])
+        .test();
+    }
 
-//     // indirect 0x70 -> 0x0250 + 1
-//     memory.store(0x0070, 0x50);
-//     memory.store(0x0071, 0x02);
-//     memory.store(0x0251, 0x0C);
-//     memory.store(0x034F, 0x0D);
+    #[test]
+    fn zeropage() {
+        const TARGET: u8 = 0x21;
+        const ADDR: u16 = 0x42;
 
-//     // LDA immediate
-//     (0..2).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 2);
-//     assert_eq!(cpu.accumulator, 0x1);
+        TestOpcodeOptions::new(OpCode::LdaZeroPage, 3, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[ADDR as u8])
+        .with_additional_values(&[(ADDR, TARGET)])
+        .test();
+    }
 
-//     // // LDA zeropage
-//     (0..3).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 4);
-//     assert_eq!(cpu.accumulator, 0x2);
+    #[test]
+    fn zeropage_x() {
+        const TARGET: u8 = 0x69;
+        const BASE_ADDR: u16 = 0x42;
+        const OFFSET: u8 = 0x3;
+        const FINAL_ADDR: u16 = (BASE_ADDR as u8).wrapping_add(OFFSET) as u16;
 
-//     // LDA zeropage + X
-//     cpu.x_index = 0x1;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 6);
-//     assert_eq!(cpu.accumulator, 0x3);
+        TestOpcodeOptions::new(OpCode::LdaZeroPageX, 4, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[BASE_ADDR as u8])
+        .with_prepare(|cpu| cpu.x_index = OFFSET)
+        .with_additional_values(&[(FINAL_ADDR, TARGET)])
+        .test();
+    }
 
-//     // LDA zeropage + X overflow
-//     cpu.x_index = 0xB5;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 8);
-//     assert_eq!(cpu.accumulator, 0x4);
+    #[test]
+    fn zeropage_x_overflow() {
+        const TARGET: u8 = 0x69;
+        const BASE_ADDR: u16 = 0x42;
+        const OFFSET: u8 = 0xFA;
+        const FINAL_ADDR: u16 = (BASE_ADDR as u8).wrapping_add(OFFSET) as u16;
 
-//     // LDX absolute
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 11);
-//     assert_eq!(cpu.accumulator, 0x5);
+        TestOpcodeOptions::new(OpCode::LdaZeroPageX, 4, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[BASE_ADDR as u8])
+        .with_prepare(|cpu| cpu.x_index = OFFSET)
+        .with_additional_values(&[(FINAL_ADDR, TARGET)])
+        .test();
+    }
 
-//     // LDA absolute + X
-//     cpu.x_index = 0x1;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 14);
-//     assert_eq!(cpu.accumulator, 0x6);
+    #[test]
+    fn absolute() {
+        const TARGET: u8 = 0x75;
+        const ADDR: u16 = 0x0457;
 
-//     // LDA absolute + X overflow
-//     cpu.x_index = 0xB5;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_ne!(cpu.accumulator, 0x7); // shouldn't be ready yet coz of page boundary
-//     cpu.run_cycle(&mut memory);
-//     assert_eq!(cpu.program_counter, 17);
-//     assert_eq!(cpu.accumulator, 0x7); // NOW it's ready
-//                                       // the current instruction must be finished
-//     assert_eq!(cpu.current_cycle, 0);
+        TestOpcodeOptions::new(OpCode::LdaAbs, 4, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&ADDR.to_le_bytes())
+        .with_additional_values(&[(ADDR, TARGET)])
+        .test();
+    }
 
-//     // LDA absolute + Y
-//     cpu.y_index = 0x2;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 20);
-//     assert_eq!(cpu.accumulator, 0x8);
+    #[test]
+    fn absolute_x() {
+        const TARGET: u8 = 0x34;
+        const BASE_ADDR: u16 = 0x0361;
+        const OFFSET: u8 = 0x12;
+        const FINAL_ADDR: u16 = BASE_ADDR.wrapping_add(OFFSET as u16);
 
-//     // LDA absolute + Y overflow
-//     cpu.y_index = 0xB6;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_ne!(cpu.accumulator, 0x9); // shouldn't be ready yet coz of page boundary
-//     cpu.run_cycle(&mut memory);
-//     assert_eq!(cpu.program_counter, 23);
-//     assert_eq!(cpu.accumulator, 0x9); // NOW it's ready
-//                                       // the current instruction must be finished
-//     assert_eq!(cpu.current_cycle, 0);
+        TestOpcodeOptions::new(OpCode::LdaAbsX, 4, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&BASE_ADDR.to_le_bytes())
+        .with_prepare(|cpu| cpu.x_index = OFFSET)
+        .with_additional_values(&[(FINAL_ADDR, TARGET)])
+        .test();
+    }
 
-//     cpu.x_index = 0x1;
-//     (0..6).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 25);
-//     assert_eq!(cpu.accumulator, 0xA);
+    #[test]
+    fn absolute_x_overflow() {
+        const TARGET: u8 = 0x34;
+        const BASE_ADDR: u16 = 0x0361;
+        const OFFSET: u8 = 0xFE;
+        const FINAL_ADDR: u16 = BASE_ADDR.wrapping_add(OFFSET as u16);
 
-//     cpu.x_index = 0xFF;
-//     (0..6).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 27);
-//     assert_eq!(cpu.accumulator, 0xB);
+        TestOpcodeOptions::new(OpCode::LdaAbsX, 5, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&BASE_ADDR.to_le_bytes())
+        .with_prepare(|cpu| cpu.x_index = OFFSET)
+        .with_additional_values(&[(FINAL_ADDR, TARGET)])
+        .test();
+    }
 
-//     cpu.y_index = 0x1;
-//     (0..5).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 29);
-//     assert_eq!(cpu.accumulator, 0xC);
+    #[test]
+    fn absolute_y() {
+        const TARGET: u8 = 0x33;
+        const BASE_ADDR: u16 = 0x0365;
+        const OFFSET: u8 = 0x12;
+        const FINAL_ADDR: u16 = BASE_ADDR.wrapping_add(OFFSET as u16);
 
-//     cpu.y_index = 0xFF;
-//     (0..5).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_ne!(cpu.accumulator, 0xD);
-//     cpu.run_cycle(&mut memory);
-//     assert_eq!(cpu.accumulator, 0xD);
-//     assert_eq!(cpu.program_counter, 31);
-// }
+        TestOpcodeOptions::new(OpCode::LdaAbsY, 4, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&BASE_ADDR.to_le_bytes())
+        .with_prepare(|cpu| cpu.y_index = OFFSET)
+        .with_additional_values(&[(FINAL_ADDR, TARGET)])
+        .test();
+    }
 
-// #[test]
-// fn ldx_test() {
-//     let mut ram = Ram::new();
-//     let mut cpu = Cpu::new();
-//     let mut memory = MemoryMapping { ram: &mut ram };
-//     cpu.program_counter = 0;
+    #[test]
+    fn absolute_y_overflow() {
+        const TARGET: u8 = 0x33;
+        const BASE_ADDR: u16 = 0x0365;
+        const OFFSET: u8 = 0xFE;
+        const FINAL_ADDR: u16 = BASE_ADDR.wrapping_add(OFFSET as u16);
 
-//     #[rustfmt::skip]
-//     let mem_state = [
-//         // LDX #1
-//         0xA2, 0x1,
-//         // LDX $89
-//         0xA6, 0x89,
-//         // LDX $89, Y       (Y = 0x01)
-//         0xB6, 0x89,
-//         // LDX $89, Y       (Y = 0xB6) (test page overflow behavior)
-//         0xB6, 0x89,
-//         // LDX 0x0489
-//         0xAE, 0x89, 0x04,
-//         // LDX 0x0489, Y    (Y = 0x01)
-//         0xBE, 0x89, 0x04,
-//         // LDX 0x0489, Y    (Y = 0xB6) (test page overflow behavior)
-//         0xBE, 0x89, 0x04,
-//     ];
-//     for (i, byte) in mem_state.into_iter().enumerate() {
-//         memory.store(i as u16, byte);
-//     }
-//     // zero page 0x89
-//     memory.store(0x0089, 0x02);
-//     // zero page 0x89 + 1
-//     memory.store(0x008A, 0x03);
-//     // zero page 0x89 + 0xB6 (0x003F), page is wrapped around
-//     memory.store(0x003F, 0x04);
-//     // absolute 0x0489
-//     memory.store(0x0489, 0x05);
-//     // absolute 0x0489 + 1
-//     memory.store(0x048A, 0x06);
-//     // absolute 0x0489 + 0xB6 (0x053F), page boundary must be crossed
-//     memory.store(0x053F, 0x07);
+        TestOpcodeOptions::new(OpCode::LdaAbsY, 5, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&BASE_ADDR.to_le_bytes())
+        .with_prepare(|cpu| cpu.y_index = OFFSET)
+        .with_additional_values(&[(FINAL_ADDR, TARGET)])
+        .test();
+    }
 
-//     // LDX immediate
-//     (0..2).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 2);
-//     assert_eq!(cpu.x_index, 0x1);
+    #[test]
+    fn indirect_x() {
+        const TARGET: u8 = 0x57;
+        const BASE_PTR: u8 = 0x77;
+        const OFFSET: u8 = 0x32;
+        const FINAL_PTR: u8 = BASE_PTR.wrapping_add(OFFSET);
+        const ADDR: u16 = 0x0432;
 
-//     // LDX zeropage
-//     (0..3).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 4);
-//     assert_eq!(cpu.x_index, 0x2);
+        TestOpcodeOptions::new(OpCode::LdaIndirectX, 6, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[BASE_PTR])
+        .with_prepare(|cpu| cpu.x_index = OFFSET)
+        .with_additional_values(&[
+            (FINAL_PTR as u16, ADDR.to_le_bytes()[0]),
+            (FINAL_PTR.wrapping_add(1) as u16, ADDR.to_le_bytes()[1]),
+            (ADDR, TARGET),
+        ])
+        .test();
+    }
 
-//     // LDX zeropage + Y
-//     cpu.y_index = 0x1;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 6);
-//     assert_eq!(cpu.x_index, 0x3);
+    #[test]
+    fn indirect_x_overflow() {
+        const TARGET: u8 = 0x57;
+        const BASE_PTR: u8 = 0x77;
+        const OFFSET: u8 = 0xFF;
+        const FINAL_PTR: u8 = BASE_PTR.wrapping_add(OFFSET);
+        const ADDR: u16 = 0x0432;
 
-//     // LDX zeropage + Y overflow
-//     cpu.y_index = 0xB6;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 8);
-//     assert_eq!(cpu.x_index, 0x4);
+        TestOpcodeOptions::new(OpCode::LdaIndirectX, 6, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[BASE_PTR])
+        .with_prepare(|cpu| cpu.x_index = OFFSET)
+        .with_additional_values(&[
+            (FINAL_PTR as u16, ADDR.to_le_bytes()[0]),
+            (FINAL_PTR.wrapping_add(1) as u16, ADDR.to_le_bytes()[1]),
+            (ADDR, TARGET),
+        ])
+        .test();
+    }
 
-//     // LDX absolute
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 11);
-//     assert_eq!(cpu.x_index, 0x5);
+    #[test]
+    fn indirect_y() {
+        const TARGET: u8 = 0xFD;
+        const PTR: u8 = 0xD7;
+        const BASE_ADDR: u16 = 0x057A;
+        const OFFSET: u8 = 0x1D;
+        const FINAL_ADDR: u16 = BASE_ADDR.wrapping_add(OFFSET as u16);
 
-//     // LDX absolute + Y
-//     cpu.y_index = 0x1;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_eq!(cpu.program_counter, 14);
-//     assert_eq!(cpu.x_index, 0x6);
+        TestOpcodeOptions::new(OpCode::LdaIndirectY, 5, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[PTR])
+        .with_additional_values(&[
+            (PTR as u16, BASE_ADDR.to_le_bytes()[0]),
+            (PTR.wrapping_add(1) as u16, BASE_ADDR.to_le_bytes()[1]),
+            (FINAL_ADDR, TARGET),
+        ])
+        .with_prepare(|cpu| cpu.y_index = OFFSET)
+        .test();
+    }
 
-//     // LDX absolute + Y overflow
-//     cpu.y_index = 0xB6;
-//     (0..4).for_each(|_| cpu.run_cycle(&mut memory));
-//     assert_ne!(cpu.x_index, 0x7); // shouldn't be ready yet coz of page boundary
+    #[test]
+    fn indirect_y_overflow() {
+        const TARGET: u8 = 0xFD;
+        const PTR: u8 = 0xD7;
+        const BASE_ADDR: u16 = 0x057A;
+        const OFFSET: u8 = 0xCE;
+        const FINAL_ADDR: u16 = BASE_ADDR.wrapping_add(OFFSET as u16);
 
-//     cpu.run_cycle(&mut memory);
-//     assert_eq!(cpu.program_counter, 17);
-//     assert_eq!(cpu.x_index, 0x7); // NOW it's ready
-
-//     // the current instruction must be finished
-//     assert_eq!(cpu.current_cycle, 0);
-// }
+        TestOpcodeOptions::new(OpCode::LdaIndirectY, 6, |cpu, _memory| {
+            assert_eq!(cpu.accumulator, TARGET);
+        })
+        .with_arguments(&[PTR])
+        .with_additional_values(&[
+            (PTR as u16, BASE_ADDR.to_le_bytes()[0]),
+            (PTR.wrapping_add(1) as u16, BASE_ADDR.to_le_bytes()[1]),
+            (FINAL_ADDR, TARGET),
+        ])
+        .with_prepare(|cpu| cpu.y_index = OFFSET)
+        .test();
+    }
+}
