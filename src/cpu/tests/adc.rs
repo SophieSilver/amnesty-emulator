361db19c56b71d58fc29842f0ff507383d8bd0dd
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-10-25 14:06:33 +0300

    style: changed name styles a bit

--- src/cpu/tests/adc.rs
+++ src/cpu/tests/adc.rs
@@ -6,7 +6,7 @@ use utils::possible_pairs_with_carry;
 
 use super::*;
 
-fn verify_adc(a: u8, b: u8, carry: bool) -> impl Fn(&mut Cpu, &mut MemoryMapping) {
+fn verify(a: u8, b: u8, carry: bool) -> impl Fn(&mut Cpu, &mut MemoryMapping) {
     // just dump everything into u32 and see what's out of range
     let unsigned_result = a as u32 + b as u32 + carry as u32;
     // first casting to i8 to have it sign extended
@@ -43,7 +43,7 @@ fn verify_adc(a: u8, b: u8, carry: bool) -> impl Fn(&mut Cpu, &mut MemoryMapping
 #[test]
 fn immediate() {
     for (a, b, carry) in possible_pairs_with_carry() {
-        TestOpcodeOptions::new(OpCode::AdcImmediate, 2, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcImmediate, 2, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.flags.set(StatusFlags::CARRY, carry);
@@ -58,7 +58,7 @@ fn zeropage() {
     for (a, b, carry) in possible_pairs_with_carry() {
         let addr = 0x25;
 
-        TestOpcodeOptions::new(OpCode::AdcZeroPage, 3, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcZeroPage, 3, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.flags.set(StatusFlags::CARRY, carry);
@@ -75,7 +75,7 @@ fn zeropage_x() {
         let base_addr = 0x25;
         let offset = 0x20;
 
-        TestOpcodeOptions::new(OpCode::AdcZeroPageX, 4, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcZeroPageX, 4, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.x_index = offset;
@@ -93,7 +93,7 @@ fn zeropage_x_overflow() {
         let base_addr = 0x85;
         let offset = 0xD0;
 
-        TestOpcodeOptions::new(OpCode::AdcZeroPageX, 4, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcZeroPageX, 4, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.x_index = offset;
@@ -110,7 +110,7 @@ fn absolute() {
     for (a, b, carry) in possible_pairs_with_carry() {
         let addr: u16 = 0x0425;
 
-        TestOpcodeOptions::new(OpCode::AdcAbsolute, 4, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcAbsolute, 4, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.flags.set(StatusFlags::CARRY, carry);
@@ -127,7 +127,7 @@ fn absolute_x() {
         let addr: u16 = 0x0425;
         let offset: u8 = 0x5A;
 
-        TestOpcodeOptions::new(OpCode::AdcAbsoluteX, 4, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcAbsoluteX, 4, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.x_index = offset;
@@ -145,7 +145,7 @@ fn absolute_y() {
         let addr: u16 = 0x0425;
         let offset: u8 = 0x5A;
 
-        TestOpcodeOptions::new(OpCode::AdcAbsoluteY, 4, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcAbsoluteY, 4, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.y_index = offset;
@@ -163,7 +163,7 @@ fn absolute_x_overflow() {
         let addr: u16 = 0x04A5;
         let offset: u8 = 0x6A;
 
-        TestOpcodeOptions::new(OpCode::AdcAbsoluteX, 5, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcAbsoluteX, 5, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.x_index = offset;
@@ -181,7 +181,7 @@ fn absolute_y_overflow() {
         let addr: u16 = 0x04A5;
         let offset: u8 = 0x6A;
 
-        TestOpcodeOptions::new(OpCode::AdcAbsoluteY, 5, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcAbsoluteY, 5, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.y_index = offset;
@@ -192,6 +192,7 @@ fn absolute_y_overflow() {
             .test();
     }
 }
+
 #[test]
 fn indirect_x() {
     for (a, b, carry) in possible_pairs_with_carry() {
@@ -200,7 +201,7 @@ fn indirect_x() {
         let final_ptr = ptr_base.wrapping_add(offset) as u16;
         let addr: u16 = 0x0458;
 
-        TestOpcodeOptions::new(OpCode::AdcIndirectX, 6, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcIndirectX, 6, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.x_index = offset;
@@ -224,7 +225,55 @@ fn indirect_y() {
         let base_addr: u16 = 0x0458;
         let final_addr = base_addr.wrapping_add(offset as u16);
 
-        TestOpcodeOptions::new(OpCode::AdcIndirectY, 5, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcIndirectY, 5, verify(a, b, carry))
+            .with_prepare(|cpu| {
+                cpu.accumulator = a;
+                cpu.y_index = offset;
+                cpu.flags.set(StatusFlags::CARRY, carry);
+            })
+            .with_arguments(&[ptr])
+            .with_additional_values(&[
+                (ptr as u16, base_addr.to_le_bytes()[0]),
+                (ptr.wrapping_add(1) as u16, base_addr.to_le_bytes()[1]),
+                (final_addr, b),
+            ])
+            .test();
+    }
+}
+
+#[test]
+fn indirect_x_overflow() {
+    for (a, b, carry) in possible_pairs_with_carry() {
+        let ptr_base: u8 = 0x3F;
+        let offset: u8 = 0xFA;
+        let final_ptr = ptr_base.wrapping_add(offset) as u16;
+        let addr: u16 = 0x0458;
+
+        TestOpcodeOptions::new(OpCode::AdcIndirectX, 6, verify(a, b, carry))
+            .with_prepare(|cpu| {
+                cpu.accumulator = a;
+                cpu.x_index = offset;
+                cpu.flags.set(StatusFlags::CARRY, carry);
+            })
+            .with_arguments(&[ptr_base])
+            .with_additional_values(&[
+                (final_ptr, addr.to_le_bytes()[0]),
+                (final_ptr.wrapping_add(1), addr.to_le_bytes()[1]),
+                (addr, b),
+            ])
+            .test();
+    }
+}
+
+#[test]
+fn indirect_y_overflow() {
+    for (a, b, carry) in possible_pairs_with_carry() {
+        let ptr: u8 = 0x3F;
+        let offset: u8 = 0xFA;
+        let base_addr: u16 = 0x0458;
+        let final_addr = base_addr.wrapping_add(offset as u16);
+
+        TestOpcodeOptions::new(OpCode::AdcIndirectY, 6, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.y_index = offset;
@@ -248,7 +297,7 @@ fn indirect_x_page_split() {
         let final_ptr = ptr_base.wrapping_add(offset);
         let addr: u16 = 0x0458;
 
-        TestOpcodeOptions::new(OpCode::AdcIndirectX, 6, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcIndirectX, 6, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.x_index = offset;
@@ -272,7 +321,7 @@ fn indirect_y_page_split() {
         let base_addr: u16 = 0x0458;
         let final_addr = base_addr.wrapping_add(offset as u16);
 
-        TestOpcodeOptions::new(OpCode::AdcIndirectY, 5, verify_adc(a, b, carry))
+        TestOpcodeOptions::new(OpCode::AdcIndirectY, 5, verify(a, b, carry))
             .with_prepare(|cpu| {
                 cpu.accumulator = a;
                 cpu.y_index = offset;
